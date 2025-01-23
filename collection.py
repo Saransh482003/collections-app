be74d4591d3189303e38dcd10e180c27330764bb
Saransh <saini.saransh03@gmail.com>
2025-01-23 21:44:15 +0530

    Last Commit

---
+++ collection.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+@app.route("/<month>")
+def hello_collection(year):
+    generation = ""
+    year = int(year)
+    if year >2000 and year < 2015:
+        generation = "Gen Z"
+    elif year >2015:
+        generation = "Gen Alpha"
+    elif year < 2000:
+        generation = "Millennials"
+    else:
+        generation = "Gen Omega"
+    return {
+        "year": year,
+        "generation": generation}
+
+
+if __name__ == "__main__":
+    app.run()
